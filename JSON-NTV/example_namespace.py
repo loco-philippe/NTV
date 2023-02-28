46e9a1270a3a4cfc0ed5641d8a5b5515e634e9bb
Philippe THOMY <philippe@loco-labs.io>
2023-03-01 00:16:40 +0100

    creation namespace

--- JSON-NTV/example_namespace.py
+++ JSON-NTV/example_namespace.py
@@ -5,9 +5,14 @@ Created on Mon Feb 27 20:37:06 2023
 @author: a179227
 """
 from namespace import Namespace
-Nroot = Namespace()
+
 Nfr = Namespace('fr.', Nroot)
 NBAN = Namespace('BAN.', Nfr)
 print(NBAN.cName)
 print(Nroot.cName)
 print(Namespace.namespaces())
+print(NBAN == Nfr)
+print(NBAN.isChild(Nfr))
+print(NBAN.isChild(Nroot))
+print(Nroot.isParent(NBAN))
+print(Nroot.isChild(NBAN))
--- JSON-NTV/namespace.py
+++ JSON-NTV/namespace.py
@@ -66,11 +66,32 @@ class Namespace():
     *static methods*
     - `load`
     '''
-    _namespaces_ =[]
+    _namespaces_ = {}
+
     
     @classmethod
     def namespaces(cls):
-        return [nam.cName for nam in cls._namespaces_]
+        return [nam.cName for nam in cls._namespaces_.values()]
+    
+    @classmethod
+    def long(cls, cName):
+        if cName in Namespace.namespaces():
+           return        
+        splitName = cName.rsplit('.', 3)
+        if len(splitName) == 1:
+            raise TypeError(cName + ' is not a valid classname')
+        if len(splitName) == 2:
+            cls.addNamespace(splitName[0]+'.')
+        xxxx
+        
+    @classmethod
+    def addNamespace(cls, name, parent=None):
+        if parent is None:
+            parent=cls._namespaces_['']
+        if not name in parent.content['namespace']:
+            raise TypeError(name + ' is not defined in ' + parent.cName)
+        cls(name, parent)
+        return
     
     def __init__(self, name='', parent=None): 
         '''
@@ -83,7 +104,7 @@ class Namespace():
         - **parent** : Namespace (default None) - parent namespace'''
         self.name = name
         self.parent = parent
-        self._namespaces_.append(self)
+        self._namespaces_[self.cName] = self
         
     @property
     def file(self):
@@ -95,7 +116,13 @@ class Namespace():
     
     @property
     def content(self):
-        return self.load(self.name, self.parent)
+        config = configparser.ConfigParser()
+        config.read(self.file)
+        configName = config['data']['name']
+        if configName != self.name:
+            raise TypeError(self.file + ' is not correct')
+        return {'type': json.loads(config['data']['type']),
+                'namespace': json.loads(config['data']['namespace'])}
     
     @property
     def cName(self):
@@ -105,8 +132,26 @@ class Namespace():
         else:
             return self.parent.cName + self.name
 
+    def isChild(self, nspace):
+        '''return the number of level between self and parent, -1 if None'''
+        parent = self.parent
+        if self == nspace: 
+            return 0
+        rang = 1
+        while not parent is None and parent != nspace:
+            rang +=1
+            parent = parent.parent
+        if parent is None:
+            return -1
+        if parent == nspace:
+            return rang
+    
+    def isParent(self, nspace):
+        '''return the number of level between self and parent, -1 if None'''
+        return nspace.isChild(self)
+    
     @staticmethod
-    def load(name, parent=None):
+    def load(name='', parent=None):
         '''return list of Type and list of Namespace included in a Namespace''' 
         config = configparser.ConfigParser()
         if parent:
@@ -127,4 +172,5 @@ class Namespace():
 class TypeError(Exception):
     ''' Type Exception'''
     # pass
-        
+
+Nroot = Namespace()        
