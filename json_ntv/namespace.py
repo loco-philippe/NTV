284d58161488455a3142ebb7db53bb6bc49d3350
Philippe THOMY <philippe@loco-labs.io>
2023-03-01 16:29:35 +0100

    end creation class NtvType, Namespace

--- json_ntv/namespace.py
+++ json_ntv/namespace.py
@@ -6,93 +6,156 @@ Created on Feb 27 22:44:05 2023
 
 The `namespace` module contains the Namespace class for NTV entity.
 """
-import configparser, json
+import configparser
+import json
+
 
 class NtvType():
     ''' type of NTV entities.
-    
+
     *Attributes :
-    
+
     - **name** : String - name of the type
     - **nspace** : Namespace - namespace associated
-    - **gentype** : Type - generic type associated
-    
+
     The methods defined in this class are :
-    
+
+    *classmethods*
+    - `types`
+    - `add`
+    - `add_ntv_type`
+
+    *dynamic values (@property)
+    - `gen_type`
+    - `long_name`
+
     *instance methods*
-    - `jsonNtvType`    
-    - `isAssociated`
-    - `isNamespace`
-    - `hasNamespace`
-    - `toStr`
+    - `isin_namespace`
     '''
-    
-    def __init__(self, name, nspace=None, gentype=None): 
-        '''
-        Type constructor.
+    _types_ = {}
+
+    @classmethod
+    def types(cls):
+        '''return the list of NtvType created'''
+        return [nam.long_name for nam in cls._types_.values()]
+
+    @classmethod
+    def add(cls, long_name):
+        '''activate and return a valid NtvType defined by the long name'''
+        if long_name in NtvType.types():
+            return cls._types_[long_name]
+        split_name = long_name.rsplit('.', 1)
+        if split_name[-1] == '':
+            raise NtvError(long_name + ' is not a valid NTVtype')
+        if len(split_name) == 1:
+            return cls.add_ntv_type(split_name[0])
+        if len(split_name) == 2:
+            nspace = Namespace.add(split_name[0]+'.')
+            return cls.add_ntv_type(split_name[1], nspace)
+        raise NtvError(long_name + ' is not a valid NTVtype')
+
+    @classmethod
+    def add_ntv_type(cls, name, nspace=None):
+        '''activate and return a valid NtvType defined by a name and a Namespace'''
+        if not nspace:
+            nspace = Namespace()
+        if not name in nspace.content['type']:
+            raise NtvError(name + ' is not defined in ' + nspace.long_name)
+        return cls(name, nspace)
+
+    def __init__(self, name, nspace=None):
+        '''NtvType constructor.
 
         *Parameters*
 
         - **name** : string - name of the Type
-        - **nspace** : Namespace (default None) - namespace associated
-        - **gentype** : Type (default None) - generic type associated'''
+        - **nspace** : Namespace (default None) - namespace associated'''
+        if not nspace:
+            nspace = Namespace()
         self.name = name
         self.nspace = nspace
-        self.gentype = gentype
-        
+        self._types_[self.long_name] = self
+
+    def __str__(self):
+        '''return string format'''
+        return self.long_name
+
+    def __repr__(self):
+        '''return classname and long name'''
+        return self.__class__.__name__ + '(' + self.long_name + ')'
+
+    @property
+    def gen_type(self):
+        '''return the generic type of the NtvType'''
+        return self.nspace.content['type'][self.name]
+
+    @property
+    def long_name(self):
+        '''return a string with the absolute name'''
+        return self.nspace.long_name + self.name
+
+    def isin_namespace(self, long_name):
+        '''return the number of level between self and nspace, -1 if None'''
+        return self.nspace.is_child(Namespace.add(long_name))
+
+
 class Namespace():
     ''' Namespace of NTV entities.
-    
+
     *Attributes :
-    
+
     - **name** : String - name of the namespace
     - **file** : string - location of the file init
     - **parent** : Namespace - parent namespace
-    
+
     The methods defined in this class are :
-    
+
     *classmethods*
     - `namespaces`
     - `add`
-    
+    - `add_namespace`
+
     *dynamic values (@property)
     - `file`
-    - `cName`
+    - `long_name`
     - `content`
-    
+
     *instance methods*
-    - `isChild`    
-    - `isParent`
+    - `is_child`
+    - `is_parent`
     '''
     _namespaces_ = {}
 
-    
     @classmethod
     def namespaces(cls):
-        return [nam.cName for nam in cls._namespaces_.values()]
-    
+        '''return the list of Namespace created'''
+        return [nam.long_name for nam in cls._namespaces_.values()]
+
     @classmethod
-    def add(cls, cName):
-        if cName in Namespace.namespaces():
-           return cls._namespaces_[cName]
-        splitName = cName.rsplit('.', 2)
-        if len(splitName) == 1:
-            raise TypeError(cName + ' is not a valid classname')
-        if len(splitName) == 2:
-            return cls.addNamespace(splitName[0]+'.')
-        if len(splitName) == 3:
-            parent = Namespace.add(splitName[0]+'.')
-            return cls.addNamespace(splitName[1]+'.', parent)
-        
+    def add(cls, long_name):
+        '''activate and return a valid Namespace defined by the long name'''
+        if long_name in Namespace.namespaces():
+            return cls._namespaces_[long_name]
+        split_name = long_name.rsplit('.', 2)
+        if len(split_name) == 1 or split_name[-1] != '':
+            raise NtvError(long_name + ' is not a valid classname')
+        if len(split_name) == 2:
+            return cls.add_namespace(split_name[0]+'.')
+        if len(split_name) == 3:
+            parent = Namespace.add(split_name[0]+'.')
+            return cls.add_namespace(split_name[1]+'.', parent)
+        raise NtvError(long_name + ' is not a valid classname')
+
     @classmethod
-    def addNamespace(cls, name, parent=None):
+    def add_namespace(cls, name, parent=None):
+        '''activate and return a valid Namespace defined by a name and a parent Namespace'''
         if parent is None:
             parent = cls._namespaces_['']
         if not name in parent.content['namespace']:
-            raise TypeError(name + ' is not defined in ' + parent.cName)
+            raise NtvError(name + ' is not defined in ' + parent.long_name)
         return cls(name, parent)
-    
-    def __init__(self, name='', parent=None): 
+
+    def __init__(self, name='', parent=None):
         '''
         Namespace constructor.
 
@@ -100,59 +163,68 @@ class Namespace():
 
         - **name** : String - name of the namespace
         - **file** : string - location of the file init
-        - **parent** : Namespace (default None) - parent namespace'''
+        - **parent** : Namespace - parent namespace'''
         self.name = name
         self.parent = parent
-        print(self.cName)
-        self._namespaces_[self.cName] = self
-        
+        self._namespaces_[self.long_name] = self
+
+    def __str__(self):
+        '''return string format'''
+        return self.long_name
+
+    def __repr__(self):
+        '''return classname and long name'''
+        return self.__class__.__name__ + '(' + self.long_name + ')'
+
     @property
     def file(self):
+        '''return the file name of the Namespace configuration'''
         config = configparser.ConfigParser()
         if self.parent:
             config.read(self.parent.file)
             return '../config/'+json.loads(config['data']['namespace'])[self.name]
         return "../config/NTV_global_namespace.ini"
-    
+
     @property
     def content(self):
+        '''return the content of the Namespace configuration'''
         config = configparser.ConfigParser()
         config.read(self.file)
-        configName = config['data']['name']
-        if configName != self.name:
-            raise TypeError(self.file + ' is not correct')
+        config_name = config['data']['name']
+        if config_name != self.name:
+            raise NtvError(self.file + ' is not correct')
         return {'type': json.loads(config['data']['type']),
                 'namespace': json.loads(config['data']['namespace'])}
-    
+
     @property
-    def cName(self):
+    def long_name(self):
         '''return a string with the absolute name'''
         if self.parent is None:
             return self.name
-        else:
-            return self.parent.cName + self.name
+        return self.parent.long_name + self.name
 
-    def isChild(self, nspace):
+    def is_child(self, nspace):
         '''return the number of level between self and parent, -1 if None'''
         parent = self.parent
-        if self == nspace: 
+        if self == nspace:
             return 0
         rang = 1
         while not parent is None and parent != nspace:
-            rang +=1
+            rang += 1
             parent = parent.parent
         if parent is None:
             return -1
         if parent == nspace:
             return rang
-    
-    def isParent(self, nspace):
+
+    def is_parent(self, nspace):
         '''return the number of level between self and parent, -1 if None'''
-        return nspace.isChild(self)
-    
-        
-class TypeError(Exception):
+        return nspace.is_child(self)
+
+
+class NtvError(Exception):
     ''' Type Exception'''
     # pass
 
-Nroot = Namespace()        
+
+Nroot = Namespace()
--- tests/tests_namespace.py
+++ tests/tests_namespace.py
@@ -8,20 +8,33 @@ The `NTV.test_namespace` module contains the unit tests (class unittest) for the
 `Namespace` and `Type` classes.
 """
 import unittest
-from namespace import Namespace
+from namespace import Namespace, NtvError, NtvType
 
 class Test_Namespace(unittest.TestCase):
     
     def test_add(self):
         liststr = ['fr.BAN.test.', 'schemaorg.', 'fr.', 'fr.IRVE.', 'fr.IRVE.']
-        for str in liststr:
-            self.assertTrue(Namespace.add(str).cName == str)
+        for nstr in liststr:
+            self.assertTrue(Namespace.add(nstr).long_name == nstr)
+    
+    def test_add_ko(self):
+        liststr = ['fr.BAN.lon', 'fr.BAN.test', 'fr', 'fr.BANN.test']
+        for nstr in liststr:
+            with self.assertRaises(NtvError):
+                Namespace.add(nstr)
 
-    @unittest.expectedFailure
-    def test_addko(self):
-        liststr = ['fr.BAN.test', 'fr.', 'fr.BANN.test', 'fr.']
-        for str in liststr:
-            self.assertTrue(Namespace.add(str).cName == str)        
-        
+class Test_NtvType(unittest.TestCase):
+    
+    def test_add(self):
+        liststr = ['fr.BAN.lon', 'year', 'fr.reg', 'fr.BAN.numero', 'fr.reg']
+        for tstr in liststr:
+            self.assertTrue(NtvType.add(tstr).long_name == tstr)
+    
+    def test_add_ko(self):
+        liststr = ['fr.BAN.test', 'fr', 'fr.BANN.lon']
+        for tstr in liststr:
+            with self.assertRaises(NtvError):
+                NtvType.add(tstr)        
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)        
