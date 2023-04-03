7c7cbcb1f210ca23ebac3cd9c2c28acbd4a5cf75
Philippe THOMY <philippe@loco-labs.io>
2023-04-04 00:01:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="json_ntv",
-    version="0.0.3",
+    version="0.0.4",
     description="JSON-NTV a semantic format for interoperability",
     long_description=long_description,
     long_description_content_type="text/markdown",
