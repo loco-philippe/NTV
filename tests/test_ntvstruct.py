fb70257f0ce9258a7fde8c7d1b0e5eb545c4e5b5
Philippe THOMY <philippe@loco-labs.io>
2023-07-04 16:16:02 +0200

    struct

--- tests/test_ntvstruct.py
+++ tests/test_ntvstruct.py
@@ -4,7 +4,7 @@ Created on Sun Apr 23 23:09:43 2023
 
 @author: a lab in the Air
 """
-from ntv import NtvList, NtvSingle, NtvSet, Ntv
+from json_ntv import NtvList, NtvSingle, Ntv
 
 class NtvStruct:
     
@@ -17,19 +17,15 @@ class NtvStruct:
         
     def setvalue(self, list_val):
         self.values = list_val
-        
-    def to_objset(self, **kwargs):
-        return NtvSet([NtvSingle(val, nam, typ) for val, nam, typ in 
-                        zip(self.values, self.fields_n, self.fields_t )], self.name).to_obj(**kwargs)
     
-    def to_objlist(self, **kwargs):
+    def to_obj(self, **kwargs):
         return NtvList([NtvSingle(val, nam, typ) for val, nam, typ in 
                         zip(self.values, self.fields_n, self.fields_t )], self.name).to_obj(**kwargs)
     
 sensor = NtvStruct('sensor', ['measure', 'date_meas:datetime'])
 sensor.setvalue([45.1, '2023-01-10'])
 
-print(sensor.to_objset())
-print(sensor.to_objset(format='obj'))
-print(sensor.to_objlist(simpleval=True))
-print(sensor.to_objlist(format='obj', simpleval=True))
+print(sensor.to_obj())
+print(sensor.to_obj(format='obj'))
+print(sensor.to_obj(simpleval=True))
+print(sensor.to_obj(format='obj', simpleval=True))
