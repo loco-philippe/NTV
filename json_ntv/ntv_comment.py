fe489ef6863d3a35e29e188ecaa25eff8d69eee0
Philippe THOMY <philippe@loco-labs.io>
2023-10-04 00:13:11 +0200

    #4

--- json_ntv/ntv_comment.py
+++ json_ntv/ntv_comment.py
@@ -8,6 +8,7 @@ The `NTV.json_ntv.ntv_comment` module contains the `NtvComment` class.
 """
 import copy
 from json_ntv.ntv import NtvSingle, NtvType, NtvList, Ntv
+from json_ntv.ntv_patch import NtvPatch
 
 class NtvComment:
     '''this class includes comments and change management methods for NTV entities :
@@ -20,8 +21,41 @@ class NtvComment:
     def __init__(self, ntv):
         ''' the parameter of the constructor is the Ntv entity'''
         self._ntv = ntv
+        self.comment = []
+        
+    def add_comment(self, context, patch=None):
+        '''add [context, patch] in the comment'''
+        patch = patch if isinstance(patch, NtvPatch) else NtvPatch(patch)
+        self.comment.add([context, patch])
+        return len(self.comment)-1
 
-    def add_comment(self, text, val=None, name=None, typ=None):
+    def reject_comment(self, all_comment=False):
+        '''delete the last or all comments'''
+        if all_comment:
+            self.comment = []
+        elif len(self.comment) > 0:
+            self.comment = self.comment[:-1]
+
+    def accept_comment(self, all_comments=True):
+        '''accept the or all comments and return the resulted NTV''' 
+        if not self.comment:
+            return self._ntv
+        apply = self.comment if all_comments else [self.comment[0]]
+        ntv = self.ntv
+        for comment in apply:
+            ntv = comment[1].exe(ntv)
+        return ntv
+    
+    def show_comment(self, context=True, comments=False):
+        ''' return a list with comments and context'''
+        if context and not comments:
+            return [comment[0] for comment in self.comments]
+        if not context and comments:
+            return [comment[1] for comment in self.comments]
+        return [{'context': comment[0], 'patch': comment[1]}
+                for comment in self.comments]
+
+    def add_comment_old(self, text, val=None, name=None, typ=None):
         '''add a comment (text) and a proposal for a new NTV entity defined by (val, name and typ)'''
         parent = self._ntv.parent
         if (val, typ, name) == (None, None, None):
@@ -44,7 +78,7 @@ class NtvComment:
         parent[parent.ntv_value.index(self._ntv)] = com_list
         return
     
-    def reject_comment(self):
+    def reject_comment_old(self):
         '''delete all the comments'''
         parent = self._ntv.parent
         if self._ntv.type_str != '$history':   
@@ -52,7 +86,7 @@ class NtvComment:
         old_ntv = self._ntv.ntv_value[0]
         parent[parent.ntv_value.index(self._ntv)] = old_ntv
 
-    def accept_comment(self):
+    def accept_comment_old(self):
         ''' replace the NTV entity by the last NTV proposal and delete all comments''' 
         parent = self._ntv.parent
         if self._ntv.type_str != '$history':   
