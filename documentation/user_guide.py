3da3e174e64081654e5fe26c9f2d6e79b0e138dd
Philippe THOMY <philippe@loco-labs.io>
2023-10-06 00:00:26 +0200

    Update user_guide.py

--- documentation/user_guide.py
+++ documentation/user_guide.py
@@ -5,11 +5,8 @@ Created on Fri Sep 30 10:09:01 2022
 This document presents all the information available about the `json-ntv` package.
 
 .. include:: ../README.md
-
 .. include:: ../json_ntv/README.md
-
 .. include:: ./README.md    
-
 .. include:: ../uses/README.md
 
 """
