b9b3adee3c2fd49e26cc596498b34d435f25e317
Philippe THOMY <philippe@loco-labs.io>
2023-11-26 00:07:20 +0100

    json schema

--- RFC/test_schema.py
+++ RFC/test_schema.py
@@ -1,4 +1,10 @@
-{   "$schema": "https://json-schema.org/draft/2020-12/schema",
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from jsonschema import validate
+
+schntv = {   "$schema": "https://json-schema.org/draft/2020-12/schema",
     "$id": "https://example.com/ntv.schema.json",
     "title": "NTV",
     "description": "schema for NTV entities",
@@ -26,3 +32,18 @@
         "if":  {"properties":{ "NTVtype": {"const": "int32"}, "NTVentity": {"const": "NtvSingle"} } },
         "then":{"properties":{ "NTVvalue":{"type": "integer"} } } }
     ] }
+
+val1 = {"NTVentity": "NtvSingle", "NTVtype": "string", "NTVname": "val1", "NTVvalue": 'truc'}
+val2 = {"NTVentity": "NtvSingle", "NTVtype": "int32", "NTVname": "val2", "NTVvalue": 3}
+val3 = {"NTVentity": "NtvSingle", "NTVtype": "object", "NTVname": "val3", "NTVvalue": {"v": 5, "r":3}}
+val4 = {"NTVentity": "NtvSingle", "NTVtype": "boolean", "NTVname": "val4", "NTVvalue": True}
+
+lis1 = {"NTVentity": "NtvList", "NTVname": "lis1", "NTVvalue": [val1, val2] }
+lis2 = {"NTVentity": "NtvList", "NTVname": "lis1", "NTVvalue": [lis1, val3] }
+
+print(validate(val1, schema=schntv))
+print(validate(val2, schema=schntv))
+print(validate(val3, schema=schntv))
+print(validate(val4, schema=schntv))
+print(validate(lis1, schema=schntv))
+print(validate(lis2, schema=schntv))
