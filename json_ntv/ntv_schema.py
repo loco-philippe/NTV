94f1ec2f5687382a651a71055d931b14703c9cab
Philippe THOMY <philippe@loco-labs.io>
2024-01-12 16:20:14 +0100

    jsonschema

--- json_ntv/ntv_schema.py
+++ json_ntv/ntv_schema.py
@@ -15,63 +15,64 @@ from json_ntv.ntv import Ntv, NtvList
 from json_ntv.ntv_patch import NtvPointer
 from json_ntv.namespace import from_file
 from jsonpointer import resolve_pointer
+from jsonschema import validate
 
 file = pathlib.Path(json_ntv.__file__).parent.parent / "RFC" / "NTV_NTVschema_namespace.ini"
 from_file(file, '$NTVschema.')
 
-def val_maxItems(ntv_data, sch) : pass
-#def val_properties(ntv_data, sch) : pass
-val_properties = None
-
-VALID = {'maxItems' : val_maxItems, 'properties': val_properties}
-
-def sh_expand(ntv_data):
+def validat(ntv_data, sch):
+    #print('    ', ntv_data, sch)
+    return validate(ntv_data, sch) is None    
     
+def sh_expand(ntv_data):    
     return {'value' : [ntv.ntv_value for ntv in ntv_data.ntv_value],
             'name'  : [ntv.ntv_name if ntv.ntv_name else None for ntv in ntv_data.ntv_value], 
             'type'  : [ntv.type_str for ntv in ntv_data.ntv_value]
             } if isinstance (ntv_data, NtvList) else {
                 'value' : ntv_data.ntv_value,
                 'name'  : ntv_data.ntv_name, 
-                'type'  : ntv_data.ntv_type}
- 
+                'type'  : ntv_data.type_str}
+
+def sh_compact(schema):
+    sch = {}
+    sch['type']  = schema['type.']  if 'type.'  in schema.keys() else {}
+    sch['name']  = schema['name.']  if 'name.'  in schema.keys() else {}
+    sch['value'] = schema['value.'] if 'value.' in schema.keys() else {}
+    sch['value'] |= {key:val for key, val in schema.items() 
+                     if not key in ['type.', 'name.', 'value.']}
+    return sch
+
 def sh_items(schema):
-    name = {}
-    typ = {}
     schema = schema['items.'] if 'items.' in schema else schema
-    if 'name.' in schema:
-        name = {'name': {'items': schema['name.']}}
-        del(schema['name.'])
-    if 'type.' in schema:
-        typ = {'type': {'items': schema['type.']}}
-        del(schema['type.'])
-    value = {'value': {'items': schema}} if schema else {}
-    js_items = {'properties' : value | name | typ }
-    return js_items
+    name = {'name': {'items': schema['name.']}} if 'name.' in schema else {}
+    typ  = {'type': {'items': schema['type.']}} if 'type.' in schema else {}
+    value = {'value': {'items': {k:v for k, v in schema.items() if not k in ['name.', 'type.']}}}
+    return {'properties' : value | name | typ }
 
-def val_maxItems(ntv_data, sch) : 
-    print('    ', ntv_data)
-    print('    ', sch)
-    return True
+def val_items(ntv_data, sch) : 
+    return validat(sh_expand(ntv_data), sh_items(sch))
     
-def val_properties(ntv_data, sch) :
-    print('    ', ntv_data)
-    print('    ', sch)
-    return True
-
-def validate(keyword, ntv_data, sch):
+def val_simple(ntv_data, sch) :
+    if not sch:
+        return True
+    #print('val_simple', ntv_data, sch)
     json_data = sh_expand(ntv_data)
-    valid = False
-    if keyword in VALID:
-        print(keyword)
-        valid = VALID[keyword](json_data['value'], sch)
-    print('validate : ', keyword, valid)
+    valid = True
+    valid &= validat(json_data['value'], sh_compact(sch)['value'])
+    valid &= validat(json_data['type'],  sh_compact(sch)['type'])
+    valid &= validat(json_data['name'],  sh_compact(sch)['name'])
+    return valid
 
+def validate_ntv(keyword, ntv_data, sch):
+    print('validate : ', keyword, ntv_data)
+    valid = val_items(ntv_data, sch['items.']) if 'items.' in sch.keys() else True
+    valid &= val_simple(ntv_data, {key: val for key, val in sch.items() if not key == 'items.'})
+    print('valide : ', valid)
+    return valid
 
 def _json(dic):
     return {(lambda k : k[1:] if k[0] == ':' else k)(key): _json(value) 
             for key, value in dic.items()} if isinstance(dic, dict) else dic
-
     
 def _pure(dic, keywords=None):
     keyw = list(keywords) if keywords else []
@@ -83,7 +84,7 @@ def navigate(data, sch):
     p_data = str(ntv_data.pointer())
     sch_p =  list(sch.keys())[0]
     mapping = {p_data : '/' + sch_p}
-    validate('global', ntv_data, _pure(sch[sch_p]))
+    validate_ntv('global', ntv_data, _pure(sch[sch_p]))
     for ntv in list(ntv_data.tree)[1:]:
         parent_ntv = ntv.pointer()[:-1]
         parent_sch = resolve_pointer(sch, mapping[str(parent_ntv)])
@@ -91,18 +92,18 @@ def navigate(data, sch):
         if 'properties.' in parent_sch:
             if ntv.ntv_name in parent_sch['properties.']:
                 mapping[new_p_data] = mapping[str(parent_ntv)] + '/properties.' + '/' + ntv.ntv_name
-                validate('properties', ntv_data['#' + new_p_data], 
+                validate_ntv('properties', ntv_data['#' + new_p_data], 
                          _pure(parent_sch['properties.'][ntv.ntv_name]))
             else:
-                validate('not include', ntv_data['#' + new_p_data], True)
+                print('not include', ntv_data['#' + new_p_data], True)
         elif 'prefixItems.' in parent_sch:
             row = NtvPointer.pointer_list(ntv.pointer()[-1])[0]
             if len(parent_sch['prefixItems.']) > row:
                 mapping[new_p_data] = mapping[str(parent_ntv)] + '/prefixItems.' + '/' + str(row)
-                validate('prefixItems', ntv_data['#' + new_p_data], 
+                validate_ntv('prefixItems', ntv_data['#' + new_p_data], 
                          _pure(parent_sch['prefixItems.'][row]))
             else:
-                validate('not include', ntv_data['#' + new_p_data], True)
+                print('not include', ntv_data['#' + new_p_data], True)
         else:
             print('pas de properties et de PrefixItems')
 
